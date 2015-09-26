08259b815101695d999ed5caa81b28b7a6d62419
mathwaali <mathwaali@users.noreply.github.com>
2015-09-26 19:58:00 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,31 @@
 ## functions do
 
 ## Write a short comment describing this function
+## makeCacheMatrix creates a vector of functions which are then used by cacheSolve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  n <- NULL
+  set <- function(y) {
+    x <<- y # set x to the value in y, x is in the parent environment 
+    n  <<- NULL # set n to Null
+  }
+  get <- function() x  # this simply gets the matrix x that has been set inside the "set" function
+  setmatrix <- function(InvMatrix) 
+    n <<- InvMatrix # put the matrix to be inverted in n. That 
+  getmatrix<- function() n # cached matrix
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Thie function Cache Solve finds inverse of a matrix input and passes it into cache
+cacheSolve <- function(x,...) {   ## z is the function makeCacheMatix - need to specify the matrix by passing it as an argument
+  m <- x$getmatrix()              ## the matrix to be inverted is obtained using getmatrix function within makeCacheMatrix
+  if(!is.null(m)) {
+    message("getting cached data") ## get cached data if m is not nukk
+    return(m)
+  }
+  A<- x$get()  # store matrix to be inverted in A
+  m <- solve(A, ...)  # calculate the inverse
+  #z$getmatrix(m)      # pass the inverse
+  m
 }
